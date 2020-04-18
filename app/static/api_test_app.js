fae287bcb0149a39d5d22bca10e610245bf688a9
Jaime Echevarria <jaime030690@gmail.com>
2020-04-18 11:14:03 -0400

    created function to test all routes

--- app/static/api_test_app.js
+++ app/static/api_test_app.js
@@ -1,5 +1,16 @@
-var url = "http://127.0.0.1:5000/api/v1.0/alcohol_production";
+var base = "http://127.0.0.1:5000";
 
-d3.json(url).then(function(data){
-    console.log(data);
+var routes = [
+    "/api/v1.0/alcohol_consumption",
+    "/api/v1.0/alcohol_production",
+    "/api/v1.0/country",
+    "/api/v1.0/reviews",
+    "/api/v1.0/traffic_related_deaths"
+];
+
+routes.forEach(function(route) {
+    d3.json(base + route).then(function(data) {
+        console.log("API route: " + route);
+        console.log(data);
+    })
 });
