212d0a5c926c23acda55f4c9118ec430ab9802b9
Topias Lemetyinen <topias.lemetyinen@gmail.com>
2020-04-22 08:08:44 -0400

    merging JS

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -12,105 +12,175 @@ var myMap = L.map("map", {
   }).addTo(myMap);
 
 
-/* Alessio Base Layers*/
-
-var consuptiondata = "../data/alcohol_consumption.json";
-var geodata = "../data/countries-hires.json"
-
-d3.json(consuptiondata, function(response){
-    console.log(response);
-  });
-
-function countrycolor(chocolate){
-    //console.log(item)
-    if (chocolate >= 1){
-        return "yellow"
-    }
-    else if (chocolate >= 2){
-        return "red"
-    }
-    else {
-        return "black"
-    }
-}
-
-function chooseColor(country) {
-    switch (country) {
-    case "Poland":
-      return "brown"; 
-    case "Germany":
-      return "red";
-    case "Italy":
-      return "orange";
-    case "France":
-      return "white";
-    case "Belgium":
-      return "purple";
-    default:
-      return "green";
-    }
-  }
+/* Alessio Base Layers */
+var consumptiondata = "../data/alcohol_consumption.json";
+  var geodata = "../data/countries-hires.json";
   
-d3.json(geodata, function(response){
-      console.log(response);
-      L.geoJson(response,{
-        style: function(apple) {
+d3.json(consumptiondata, function(response){
+    console.log("this is consumption data")
+    console.log(response);
+    do_this(response);
+});
+
+function chooseColor(place) {
+  if(place <=2)
+  {
+    return "#ffffffe3";
+  }else if (place <=5)
+  {
+    return "#bb9ef5e3";
+  }
+  else if (place <=7)
+  {
+    return "#9c70f3e3";
+  }
+  else if (place <=10)
+  {
+    return "#7d45eae3";
+  }
+  else if (place <=12)
+  {
+    return "#5600ff";
+  }
+  else if (place <=15)
+  {
+    return "tomato";
+  }
+  else{
+    return "black";
+  }
+};
+
+function do_this(consumption){
+  d3.json(geodata, function(response){
+    
+    for(var i =0; i<response.features.length; i++){
+      country = response.features[i].properties.SOVEREIGNT
+      for(var z =0; z<consumption.length; z++){
+        if(country==consumption[z].country_name 
+        || country == "United States of America"
+        || country == "Venezuela"
+        || country == "Russia"
+        || country == "Czech Republic"
+        || country == "Republic of Serbia"
+        || country == "Kosovo"
+        || country == "Macedonia"
+        || country == "Moldova"
+        || country == "Bolivia"
+        || country == "United Republic of Tanzania"
+        || country == "Democratic Republic of the Congo"
+        || country == "Republic of the Congo"
+        || country == "Ivory Coast"
+        || country == "South Sudan"
+        || country == "Somaliland"
+        || country == "Iran"
+        || country == "Palestine"
+        || country == "Northern Cyprus"
+        || country == "Western Sahara"
+        || country == "Guinea Bissau"
+        || country == "South Korea"
+        || country == "North Korea"
+        || country == "Vietnam"
+        || country == "Laos" 
+        || country == "Syria"
+        || country == "Antarctica"
+        || country == "Taiwan"
+        || country == "East Timor"
+        || country == "Brunei"
+        || country == "Swaziland"
+        || country == "Kashmir"
+        || country == "Vatican"
+        || country == "San Marino"
+        || country == "The Bahamas"
+        || country == "Federated States of Micronesia"
+        || country == "Monaco"       
+        ){
+          response.features[i].properties["both_sexes"] = consumption[z].both_sexes;
+          response.features[i].properties["male"] = consumption[z].male;
+          response.features[i].properties["female"] = consumption[z].female;
+          response.features[i].properties["year"] = consumption[z].year;
           
-          return {
-            color: "white",
-            // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-            fillColor: chooseColor(apple.properties.SOVEREIGNT),
-            fillOpacity: 0.5,
-            weight: 1.5
-          };
         }
+        
+      } 
+    }
+    do_that(response);
+  })
+};
+function do_that(consumption){
+    console.log("this is consumption")
+    console.log(consumption);
+  for(var i =0; i<consumption.features.length; i++){
+
+    if(consumption.features[i].properties.both_sexes == "undefined")
+  {
+    console.log(consumption.features[i].properties.SOVEREIGNT);
+  }
+  else{
+    //  console.log(consumption.features[i].properties.SOVEREIGNT);
+  }
+  };
+  L.geoJson(consumption, {
+   
+    style: function(feature) {
+      return {
+        color: "white",
+        fillColor: chooseColor(feature.properties.both_sexes),
+        fillOpacity: .7,
+        weight: 1.5
+      };
       
-      })
+    },
+    onEachFeature: function(feature, layer) {
+      layer.on({
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.7
+          });
+        },
+        click: function(event) {
+          console.log(feature.properties.SOVEREIGNT);
+          //myMap.fitBounds(event.target.getBounds());
+        }
+        
+
+      });
+      layer.bindPopup("<h4>" + "Per capita consumption is " + feature.properties.both_sexes + "</h4> <hr> <h5>" + feature.properties.SOVEREIGNT + "</h5>");
+
+    }
   }).addTo(myMap);
 
-  // Store API query variables
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var colors = ["#ffffffe3", "#bb9ef5e3", "#9c70f3e3","#7d45eae3","#5600ff","tomato"];
+    var colorscodes = ["0-2", "2.1-5","5.5-7","7.1-10","10.1-12", "12.1+"];
+    var labels = []
 
-  var baseURL = "data/winemag-data-130k-v2.csv";
-  
-  //var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
-  //var complaint = "&complaint_type=Rodent";
-  //var limit = "&$limit=10000";
-  
-  // Assemble API query URL
-  //var url = baseURL + date + complaint + limit;
-  
-  // Grab the data with d3
-  d3.csv(baseURL, function(response) {
-      console.log(response);
-    // Create a new marker cluster group
-    var markers = L.markerClusterGroup();
-  
-    // Loop through data
-    for (var i = 0; i < response.length; i++) {
-  
-      // Set the data location property to a variable
-      var location = response[i].location;
-  
-      // Check for location property
-      if (location) {
-  
-        // Add a new marker to the cluster group and bind a pop-up
-        markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-          .bindPopup(response[i].descriptor));
-      }
-  
+    for(var i = 0; i < colors.length; i ++){
+        labels.push("<i style=\"background-color: " + colors[i] + "\">&nbsp;&nbsp;&nbsp;</i><i>" + colorscodes[i] + "</i><br>");
     }
-  
-    // Add our marker cluster layer to the map
-    myMap.addLayer(markers);
-  
-  });
+
+    div.innerHTML += "" + labels.join("") + "";
+    return div;
+  };
+
+  // Adding legend to the map
+  legend.addTo(myMap);
+};
 
 /* Will Bubble Layer*/
 
 // Country data
 var jsonData;
-var queryUrl = "data/traffic_related_deaths.json"
+var queryUrl = "../data/traffic_related_deaths.json"
 d3.json(queryUrl, function (data) {
   jsonData = data;
   
